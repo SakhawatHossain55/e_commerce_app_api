8b902cf7ad4e9f781f2dabd6cd166fee9db3eb0f
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-10-03 08:54:05 +0600

    [ADD] order api

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routers/user");
 const authRoute = require("./routers/auth");
+const productRoute = require("./routers/product");
+const cartRoute = require("./routers/cart");
+const orderRoute = require("./routers/order");
 
 dotenv.config();
 
@@ -18,6 +21,9 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running");
--- models/Order.js
+++ models/Order.js
@@ -1,3 +1,4 @@
+
 const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema(
@@ -5,13 +6,18 @@ const OrderSchema = new mongoose.Schema(
     userId: { type: String, required: true },
     products: [
       {
-        productId: { type: String },
-        quantity: { type: Number, default: 1 },
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
       },
     ],
     amount: { type: Number, required: true },
-    address: {type: Object, required: true },
-    status: { type: String, default: "pending"}
+    address: { type: Object, required: true },
+    status: { type: String, default: "pending" },
   },
   { timestamps: true }
 );
