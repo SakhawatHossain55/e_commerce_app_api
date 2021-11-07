3db8027356f1ef8112a9264de373d7a9bb4b7433
sakhawathossain <sakhawathossain7969@gmail.com>
2021-11-07 15:02:29 +0600

    test

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const authRoute = require("./routers/auth");
 const productRoute = require("./routers/product");
 const cartRoute = require("./routers/cart");
 const orderRoute = require("./routers/order");
+const stripeRoute = require("./routers/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -18,14 +20,15 @@ mongoose
     console.log(err);
   });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running");
 });
-
